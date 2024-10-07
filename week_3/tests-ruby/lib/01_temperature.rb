884d86441c4b3ecc0533a96bc6bc8f61331401a8
hellijah <thierry.corlieto@gmail.com>
2024-10-07 18:31:09 +0200

    modif 01 & 02

--- week_3/tests-ruby/lib/01_temperature.rb
+++ week_3/tests-ruby/lib/01_temperature.rb
@@ -1,9 +1,9 @@
 # Converts Fahrenheit to Celsius
 def ftoc(fahrenheit)
     return ((fahrenheit - 32) * 5.0 / 9.0).to_f
-  end
+end
   
-  # Converts Celsius to Fahrenheit
-  def ctof(celsius)
+# Converts Celsius to Fahrenheit
+def ctof(celsius)
     return ((celsius * 9.0 / 5.0) + 32).to_f
-  end
+end
--- week_3/tests-ruby/lib/02_calculator.rb
+++ week_3/tests-ruby/lib/02_calculator.rb
@@ -1,5 +1,5 @@
 def add(a, b)
-    return a + b
+  return a + b
 end
 
 def subtract(a, b)
