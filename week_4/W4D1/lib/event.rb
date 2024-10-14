c1f07a38117a4b402b39d23da004c68cda3ceaf0
hellijah <thierry.corlieto@gmail.com>
2024-10-14 17:03:22 +0200

    update classs event

--- week_4/W4D1/lib/event.rb
+++ week_4/W4D1/lib/event.rb
@@ -9,13 +9,30 @@ class Event
     @title = title
     @attendees = attendees
   end
-
-  def
-
+  
+  def postpone_24h
+    @start_date += 86400 # 86400 seconds = 24 hours
   end
 
-  def
+  def end_date
+    @start_date + (@duration * 60) # convert duration in seconds
+  end
 
+  def past?
+    @start_date < Time.now
+  end
+
+  def upcoming_in_30_minutes?
+    @start_date <= Time.now + 1800 && !past? # 1800 seconds = 30 minutes
+  end
+
+  # Méthode pour afficher l'événement
+  def display_event
+    puts "Titre : #{@title}"
+    puts "Date de début : #{@start_date.strftime("%H:%M:%S %d/%m/%Y")}"
+    puts "Durée : #{@duration} minutes"
+    puts "Invités : #{@attendees.join(', ')}"
+    puts "Date de fin : #{end_date.strftime("%H:%M:%S %d/%m/%Y")}"
   end
 
 end
