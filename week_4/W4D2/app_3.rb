aebb0d11ee4058595e15c535a09306519e9529da
hellijah <thierry.corlieto@gmail.com>
2024-10-15 19:01:53 +0200

    app 3.0 working

--- week_4/W4D2/app_3.rb
+++ week_4/W4D2/app_3.rb
@@ -4,8 +4,39 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-# change player name color
-def colored(text, color_code)
-  "\e[#{color_code}m#{text}\e[0m"
+
+puts "--------------------------------------------------"
+puts "| Bienvenue sur 'ILS VEULENT TOUS MA POO' !       |"
+puts "| Le but du jeu est d'être le dernier survivant ! |"
+puts "--------------------------------------------------"
+
+# Init game
+puts "Quel est ton prénom ?"
+print "> "
+player_name = gets.chomp
+game = Game.new(player_name)
+
+# Fight loop
+while game.is_still_ongoing?
+  game.show_players
+  game.menu
+  
+  print "> "
+  choice = gets.chomp
+  game.menu_choice(choice)
+  
+  # enemies strike back 
+  if game.is_still_ongoing?
+    puts "\nLes autres joueurs t'attaquent !" 
+    game.enemies_attack
+  end
+
+  # waiting for key entry
+  puts "\nAppuie sur entrée pour continuer"
+  gets.chomp
+  # clean terminal
+  system("clear")
 end
 
+# end game
+game.end
--- week_4/W4D2/lib/game.rb
+++ week_4/W4D2/lib/game.rb
@@ -1,6 +1,11 @@
 class Game
   attr_accessor :HumanPlayer, :enemies
 
+# change player name color
+# def colored(text, color_code)
+#   "\e[#{color_code}m#{text}\e[0m"
+# end
+
   def initialize(player_name)
     @human_player = HumanPlayer.new(player_name)
     @enemies = []
@@ -9,11 +14,62 @@ class Game
   end
 
   def kill_player(player)
-    @enemies.delete(player) # Supprime le joueur mort de @enemies
+    @enemies.delete(player) # delete dead player from array
   end
 
+  def is_still_ongoing?
+    @human_player.life_points > 0 && !@enemies.empty?
+  end
 
+  def show_players
+    puts @human_player.show_state
+    puts "Il reste #{@enemies.size} ennemis."
+  end
 
+  def menu
+    puts "\n Choix possible"
+    puts "a -> chercher une nouvelle arme"
+    puts "s -> chercher un health pack"
+    puts "attaquer un joueur :"
 
+    #  show ennemies array as a choice from 0 to 3 if alive
+    @enemies.each_with_index do |enemy, index|
+      # if enemy.life_points > 0
+        puts "#{index} -> #{enemy.name}"
+        puts "#{enemy.show_state}"
+      # end
+    end
+  end
+
+  def menu_choice(choice)
+    case choice
+    when "a"
+      @human_player.search_weapon
+    when "s"
+      @human_player.search_health_pack
+    else
+      #  define index of alive ennemies as choice
+      index = choice.to_i # convert to integer
+      if index >= 0 && index < @enemies.size && @enemies[index].life_points > 0
+        @human_player.attacks(@enemies[index])
+        kill_player(@enemies[index]) if @enemies[index].life_points <= 0 # check if ennemy is dead
+      end
+    end
+  end
+
+  def enemies_attack # every living enemies attack the human player
+    @enemies.each do |enemy|
+      enemy.attacks(@human_player) if enemy.life_points > 0
+    end
+  end
+
+  def end
+    puts "GAME OVER"
+    if @human_player.life_points > 0
+      puts "BRAVO ! TU AS GAGNE !"
+    else
+      puts "Loser ! Tu as perdu !"
+    end
+  end
 
 end
