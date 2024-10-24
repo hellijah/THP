7a392dfa065375f23f26828e62abcd40cd0da1ef
hellijah <thierry.corlieto@gmail.com>
2024-10-24 15:19:41 +0200

    airbnb_dog upload

--- week_5/W5D4/projets/airbnb_dog/db/seeds.rb
+++ week_5/W5D4/projets/airbnb_dog/db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create cities
+paris = City.create(city_name: "Paris")
+lyon = City.create(city_name: "Lyon")
+
+# Create dogsitters
+dogsitter1 = Dogsitter.create(name: "Alice", city: paris)
+dogsitter2 = Dogsitter.create(name: "Bob", city: lyon)
+
+# Create dogs
+dog1 = Dog.create(name: "Fido", city: paris)
+dog2 = Dog.create(name: "Rex", city: lyon)
+
+# Create strolls
+Stroll.create(dogsitter: dogsitter1, dog: dog1)
+Stroll.create(dogsitter: dogsitter2, dog: dog2)
